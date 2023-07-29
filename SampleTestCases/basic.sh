fbc45a3d26c77fb3bb886adda18bad162bddfc97
Shalin Bhavsar <35373379+shalin24999@users.noreply.github.com>
2023-07-29 16:46:06 -0700

    Add files via upload

---
+++ SampleTestCases/basic.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python basic.py input1.txt b_output1.txt
+# python basic.py "$1" "$2"
---
+++ SampleTestCases/efficient.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# python efficient.py input1.txt e_output1.txt
+python efficient.py "$1" "$2"
